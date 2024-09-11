977a2484cd2433352eca838a632490fa66274db2
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2024-09-12 01:53:01 +0200

    feat: add docker & autostart daemon on demand

--- home/jannis/darwin.nix
+++ home/jannis/darwin.nix
@@ -6,6 +6,21 @@
   };
 
   programs.fish.enable = true;
+  programs.fish.functions = {
+    docker = {
+      body = ''
+        function start_docker_if_needed
+          if not test -S ~/.colima/default/docker.sock
+            echo "Starting Colima..."
+            colima start
+          end
+        end
+
+        start_docker_if_needed
+        command docker $argv
+      '';
+    };
+  };
   # fucks up starship integration
   # programs.fish.interactiveShellInit = ''
   #   zellij
@@ -13,16 +28,19 @@
   programs.starship.enable = true;
   programs.starship.enableFishIntegration = true;
 
-  home.packages = with pkgs; [
-    slack
-    whatsapp-for-mac
-    discord
-    teams
-    qbittorrent
-    spotify
-    tableplus
-    wezterm
-    lazygit
-    zellij
+  home.packages = [
+    pkgs.slack
+    pkgs.whatsapp-for-mac
+    pkgs.discord
+    pkgs.teams
+    pkgs.qbittorrent
+    pkgs.spotify
+    pkgs.tableplus
+    pkgs.wezterm
+    pkgs.lazygit
+    pkgs.zellij
+    pkgs.ripgrep
+    pkgs.docker
+    pkgs.colima
   ];
 }
