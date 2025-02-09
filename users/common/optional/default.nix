579179846a9292b5bad7fa4ca5042d33a64d8afc
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2025-02-09 22:13:14 +0100

    refactor: move some modules around

--- users/common/optional/default.nix
+++ users/common/optional/default.nix
@@ -1,17 +1,17 @@
 { lib', ... }:
 {
   imports = [
-    ./fish.nix
-    ./bash.nix
-    ./starship.nix
-    ./direnv.nix
-    ./docker.nix
-    ./podman.nix
-    ./social.nix
-    ./aerospace
-    ./firefox.nix
-    ./gh.nix
-    ./sops.nix
+    (lib'.relativeToRoot "modules/home/fish.nix")
+    (lib'.relativeToRoot "modules/home/bash.nix")
+    (lib'.relativeToRoot "modules/home/starship.nix")
+    (lib'.relativeToRoot "modules/home/direnv.nix")
+    (lib'.relativeToRoot "modules/home/docker.nix")
+    (lib'.relativeToRoot "modules/home/podman.nix")
+    (lib'.relativeToRoot "modules/home/social.nix")
+    (lib'.relativeToRoot "modules/home/aerospace")
+    (lib'.relativeToRoot "modules/home/firefox.nix")
+    (lib'.relativeToRoot "modules/home/gh.nix")
+    (lib'.relativeToRoot "modules/home/sops.nix")
     (lib'.relativeToRoot "modules/home/wezterm.nix")
     (lib'.relativeToRoot "modules/home/ghostty.nix")
     (lib'.relativeToRoot "modules/home/tmux.nix")
