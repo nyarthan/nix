8ce67a103af4c203f663af599af520c640600af1
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2025-02-09 22:42:46 +0100

    feat: some starship settings

--- modules/home/starship.nix
+++ modules/home/starship.nix
@@ -17,8 +17,25 @@ lib'.mkCustomModule [ name ] inputs (
     config = lib.mkIf cfg.enable {
       programs.starship = {
         enable = true;
+
         enableFishIntegration = lib.mkIf config.custom.fish.enable true;
         enableBashIntegration = lib.mkIf config.custom.bash.enable true;
+
+        settings = {
+          add_newline = false;
+          character = {
+            success_symbol = "[󱞩](bold green)";
+            error_symbol = "[󱞩](bold red)";
+          };
+
+          nix_shell = {
+            format = "󰇙 [$symbol $name $state ]($style)";
+            symbol = "󱄅";
+            impure_msg = "[\\(impure\\)](yellow)";
+            pure_msg = "[\\(pure\\)](green)";
+            unknown_msg = "[\\(unknown\\)](yellow)";
+          };
+        };
       };
 
       home.packages = [ pkgs.nodejs_20 ];
