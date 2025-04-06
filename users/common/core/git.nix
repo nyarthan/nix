840949db569f24fe650d338abb16496714e45fdb
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2025-04-06 18:37:33 +0200

    fix: git config

--- users/common/core/git.nix
+++ users/common/core/git.nix
@@ -1,3 +1,5 @@
+# https://nix-community.github.io/home-manager/options.xhtml#opt-programs.git.enable
+
 let
   name = "git";
 in
@@ -14,12 +16,26 @@ lib'.mkCustomModule [ name ] inputs (
     };
 
     config = lib.mkIf cfg.enable {
+      home.file.".gitconfig".text = ''
+        [init]
+          defaultBranch = master
+
+        [user]
+          name = Jannis Morgenstern
+          email = jannis.n.morgenstern@gmail.com
+
+        [filter "lfs"]
+          clean = git-lfs clean -- %f
+          smudge = git-lfss smudge -- %f
+          process = git-lfs filter-process
+          required = true
+
+        [rerere]
+          enabled = true
+      '';
+
       programs.git = {
-        userEmail = "jannis.n.morgenstern@gmail.com";
-        userName = "Jannis Morgenstern";
         enable = true;
-        lfs.enable = true;
-        maintenance.enable = true;
       };
     };
   }
